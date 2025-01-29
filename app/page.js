db566f375d6bd4a7f1bf845e29f18cf81d4fe291
JApitpit <Jianpitpit@gmail.com>
2025-01-29 08:11:57 -0700

    dog page link

--- app/page.js
+++ app/page.js
@@ -7,6 +7,9 @@ export default function HomePage() {
       <p>
         Go to the <Link href="/week-2">Week 2 Page</Link>
       </p>
+      <p>
+        Go to the <Link href="/dogs">Dogs Page</Link>
+      </p>
     </main>
 
     /*<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
