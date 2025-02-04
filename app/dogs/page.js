ed3ef5282683123425b7c12482bc0f763ef53658
JApitpit <Jianpitpit@gmail.com>
2025-02-04 14:34:40 -0700

    Added Scooby to list of Dogs

--- app/dogs/page.js
+++ app/dogs/page.js
@@ -11,7 +11,7 @@ export default function Page()
             </Dogs>
             <Dogs name="Moo Deng" dog="Hippo Dog" img="https://imgs.search.brave.com/mQSMInyn37ipIP2rE3Xh6y28x5h6mvQWOKxM1s04Lys/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMubGlmZXN0eWxl/YXNpYS5jb20vd3At/Y29udGVudC91cGxv/YWRzL3NpdGVzLzMv/MjAyNC8wOS8xMzE2/MjMyMC80NTkxMTgw/NjNfNTM5NTk3MTQ1/MjQ3MDQ3Xzg4NTM3/NDAzNTgyODg1OTAz/Mzlfbi01MDB4Mzc1/LmpwZw">
             </Dogs>
-            <Dogs>
+            <Dogs name="Scooby" dog="Coward Dog" img="https://imgs.search.brave.com/F3IHrK40pwWuuGHuDTxIdWHyEyQa3QCvnGYOdKCmI1M/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzhmL2Qx/L2ViLzhmZDFlYjQ0/YjM0YzI2Yjk4YjQz/ZWQwNjgwMWFiNjVj/LmpwZw">
             </Dogs>
         </div>
     );
