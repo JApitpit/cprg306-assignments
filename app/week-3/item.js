3c05477b7b7a02f8a61935fe8bdd56c906b808c9
JApitpit <Jianpitpit@gmail.com>
2025-02-04 15:11:25 -0700

    changed header tags to p tags

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -3,8 +3,8 @@ export default function Item({ name, quantity, category }) {
     return (
         <ul className="justify-center">
             <li className="p-2 border-b flex rounded-lg bg-[#00203FFF] shadow-md border-8 border-[#00203FFF] m-4 flex-col text-center">
-                <h className="font-semibold text-[#ADEFD1FF]">{name}</h>
-                <h className="text-[#ADEFD1FF]">buy {quantity} in {category}</h>
+                <p className="font-semibold text-[#ADEFD1FF]">{name}</p>
+                <p className="text-[#ADEFD1FF]">buy {quantity} in {category}</p>
             </li>
         </ul>
     );
