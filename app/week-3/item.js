277cb9b00edfc4587286d5358a40e5bb2705fc04
JApitpit <Jianpitpit@gmail.com>
2025-02-04 12:40:24 -0700

    did some final css

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,8 +1,8 @@
 
 export default function Item({ name, quantity, category }) {
     return (
-        <ul>
-            <li className="p-2 border-b flex rounded-lg bg-[#00203FFF] shadow-md border-8 border-[#00203FFF] m-4">
+        <ul className="justify-center">
+            <li className="p-2 border-b flex rounded-lg bg-[#00203FFF] shadow-md border-8 border-[#00203FFF] m-4 flex-col text-center">
                 <h className="font-semibold text-[#ADEFD1FF]">{name}</h>
                 <h className="text-[#ADEFD1FF]">buy {quantity} in {category}</h>
             </li>
